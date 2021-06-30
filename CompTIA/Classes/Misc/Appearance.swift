e53131b3dba3d24e3ee488a810b06766c0be2965
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-06-30 14:42:18 +0300

    build 1

---
+++ CompTIA/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 252, green: 254, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 33, green: 79, blue: 100)
+    static let mainColorAlpha = UIColor(integralRed: 33, green: 79, blue: 100, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 255, green: 171, blue: 75)
+    
+    static let progress1Color = UIColor(integralRed: 33, green: 79, blue: 100)
+    static let progress2Color = UIColor(integralRed: 255, green: 171, blue: 75)
+    static let progress3Color = UIColor(integralRed: 55, green: 160, blue: 171)
+}
