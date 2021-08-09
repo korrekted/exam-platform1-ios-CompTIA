3a3cfe1c4030ca5e9c8a3c5b6ff2ca86267e1983
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-08-09 12:33:03 +0300

    build 2

--- CompTIA/Classes/Presentation/Splash/SplashView.swift
+++ CompTIA/Classes/Presentation/Splash/SplashView.swift
@@ -58,7 +58,7 @@ private extension SplashView {
             .textAlignment(.center)
         
         let attrs2 = TextAttributes()
-            .textColor(Appearance.mainColor)
+            .textColor(Appearance.progress2Color)
             .font(Fonts.SFProRounded.bold(size: 45.scale))
             .lineHeight(53.scale)
             .textAlignment(.center)
