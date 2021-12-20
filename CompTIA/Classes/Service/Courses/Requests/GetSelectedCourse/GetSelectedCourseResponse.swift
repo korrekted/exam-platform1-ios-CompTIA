7867436501a76179f7b963ccb9dbcd8e8366f370
akonst17 <akonst17@gmail.com>
2021-12-20 17:56:24 +0300

    check list; build 6

--- CompTIA/Classes/Service/Courses/Requests/GetSelectedCourse/GetSelectedCourseResponse.swift
+++ CompTIA/Classes/Service/Courses/Requests/GetSelectedCourse/GetSelectedCourseResponse.swift
@@ -1,34 +1,34 @@
 //
 //  GetSelectedCourseResponse.swift
-//  CompTIA
+//  Nursing
 //
-//  Created by Andrey Chernyshev on 26.11.2021.
+//  Created by Andrey Chernyshev on 18.11.2021.
 //
 
 final class GetSelectedCourseResponse {
-     static func map(from response: Any) -> Course? {
-         guard
-             let json = response as? [String: Any],
-             let data = json["_data"] as? [String: Any],
-             let courseJSON = data["current_course"] as? [String: Any]
-         else {
-             return nil
-         }
-
-         guard
-              let id = courseJSON["id"] as? Int,
-              let name = courseJSON["name"] as? String,
-              let subTitle = courseJSON["sub"] as? String,
-              let isMain = courseJSON["main"] as? Bool,
-              let sort = courseJSON["sort"] as? Int
-          else {
-              return nil
-          }
-
-          return Course(id: id,
-                        name: name,
-                        subTitle: subTitle,
-                        isMain: isMain,
-                        sort: sort)
-     }
- }
+    static func map(from response: Any) -> Course? {
+        guard
+            let json = response as? [String: Any],
+            let data = json["_data"] as? [String: Any],
+            let courseJSON = data["current_course"] as? [String: Any]
+        else {
+            return nil
+        }
+        
+        guard
+            let id = courseJSON["id"] as? Int,
+            let name = courseJSON["name"] as? String,
+            let subTitle = courseJSON["sub"] as? String,
+            let isMain = courseJSON["main"] as? Bool,
+            let sort = courseJSON["sort"] as? Int
+        else {
+            return nil
+        }
+        
+        return Course(id: id,
+                      name: name,
+                      subTitle: subTitle,
+                      isMain: isMain,
+                      sort: sort)
+    }
+}
