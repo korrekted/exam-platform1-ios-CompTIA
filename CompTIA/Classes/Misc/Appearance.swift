360b3d43cbce2cb4f5d3d289cc7c5f8e91fe19c9
akonst17 <akonst17@gmail.com>
2022-07-13 13:22:20 +0300

    новые фичи из Nursing; build 13

--- CompTIA/Classes/Misc/Appearance.swift
+++ CompTIA/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 252, green: 254, blue: 255)
-    
     static let mainColor = UIColor(integralRed: 33, green: 79, blue: 100)
     static let mainColorAlpha = UIColor(integralRed: 33, green: 79, blue: 100, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 255, green: 171, blue: 75)
-    
-    static let progress1Color = UIColor(integralRed: 33, green: 79, blue: 100)
-    static let progress2Color = UIColor(integralRed: 255, green: 171, blue: 75)
-    static let progress3Color = UIColor(integralRed: 55, green: 160, blue: 171)
-    
+    static let secondColor = UIColor(integralRed: 255, green: 171, blue: 75)
+    static let thirdColor = UIColor(integralRed: 55, green: 160, blue: 171)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 252, green: 254, blue: 255)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
--- CompTIA/Classes/Misc/GlobalDefinitions.swift
+++ CompTIA/Classes/Misc/GlobalDefinitions.swift
@@ -12,11 +12,12 @@ final class GlobalDefinitions {
     static let sdkDomainUrl = "https://analytics.korrekted.com"
     static let sdkApiKey = "9E8tuLwaJ3OxNthr"
 
-    static let applicationTag = "EXAM_CompTIA"
+    static let applicationTag = "EXAM_MILITARY"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
+    static let appStoreUrl = "https://apps.apple.com/app/comptia-a-security-prep/id1572747755"
     static let appleAppID = "1572747755"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
