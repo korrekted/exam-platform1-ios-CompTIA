57660711c6766c14ffdf9c47d9c2672c7c1f97e3
akonst17 <akonst17@gmail.com>
2022-07-18 11:23:41 +0300

    fix amplitude log; build 14

--- CompTIA/Classes/Misc/GlobalDefinitions.swift
+++ CompTIA/Classes/Misc/GlobalDefinitions.swift
@@ -12,7 +12,7 @@ final class GlobalDefinitions {
     static let sdkDomainUrl = "https://analytics.korrekted.com"
     static let sdkApiKey = "9E8tuLwaJ3OxNthr"
 
-    static let applicationTag = "EXAM_MILITARY"
+    static let applicationTag = "EXAM_CompTIA"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
