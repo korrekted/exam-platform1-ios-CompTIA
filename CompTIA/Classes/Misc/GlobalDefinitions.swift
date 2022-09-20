35956e6b9553cd1536815880707497dcdec469f9
akonst17 <akonst17@gmail.com>
2022-09-20 14:43:56 +0300

    интеграция sdk 2.0; новые урлы; build 18

--- CompTIA/Classes/Misc/GlobalDefinitions.swift
+++ CompTIA/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams2.quantresear.ch"
+    static let domainUrl = "https://exams2.mastrapi.com"
     static let apiKey = "Cjw5UyCSSpL2CnjV"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "9E8tuLwaJ3OxNthr"
 
     static let applicationTag = "EXAM_CompTIA"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "9E8tuLwaJ3OxNthr"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "sIEaRTvrgNQNzDq0"
 
     static let appStoreUrl = "https://apps.apple.com/app/comptia-a-security-prep/id1572747755"
     static let appleAppID = "1572747755"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
